18768acd970e29d45e7d49e833c05d5246209685
BBeerBear <xiongx659@gmail.com>
2024-01-28 09:50:09 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Contact from './pages/Contact';
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: '/portfolio',
     element: <Root />,
     children: [
       { path: '/', element: <Home /> },
